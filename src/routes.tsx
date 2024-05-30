23f22d5bfb8fd2b9fbd00c709966e959f3e0a0aa
Mikael Malm <mikaelmalm91@gmail.com>
2024-05-30 23:04:21 +0200

    feat: implement new views

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,38 @@
+import { useEffect, lazy, Suspense } from "react";
 import { Outlet, createBrowserRouter, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import Loader from "./components/Loader";
+import { Container } from "@mui/material";
+import Header from "./components/Header";
 
-import Home from "./pages/Home";
-import Show from "./pages/Show";
-import NotFound from "./pages/NotFound";
-
+// We are using lazy loading to load the pages only when they are needed
+// This will help users with a slower internet connection, as they will not have to download all the pages at once
+const HomeView = lazy(() => import("./pages/Home"));
+const ShowView = lazy(() => import("./pages/Show"));
+const NotFoundView = lazy(() => import("./pages/NotFound"));
 
 // This wrapper ensures that the page scrolls to the top when the route changes
 function RouteWrapper() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    // We only want smooth scrolling when the user is going to the home page
+    const behavior = pathname === "/" ? "smooth" : "auto";
+    setTimeout(() => window.scrollTo({ top: 0, behavior }));
   }, [pathname]);
 
-  return <Outlet />;
+  return (
+    <Suspense
+      fallback={
+        <Container>
+          <Loader size="large" />
+        </Container>
+      }
+    >
+      <Header />
+
+      <Outlet />
+    </Suspense>
+  );
 }
 
 const router = createBrowserRouter([
@@ -23,15 +41,15 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <Home />,
+        element: <HomeView />,
       },
       {
         path: `/:id`,
-        element: <Show />,
+        element: <ShowView />,
       },
       {
         path: "*",
-        element: <NotFound />,
+        element: <NotFoundView />,
       },
     ],
   },
