441415bfd00cc36d0db8d937a139eaa5a0128f3b
Mikael Malm <mikaelmalm91@gmail.com>
2024-05-30 23:04:08 +0200

    feat: add not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,23 @@
+import { Box, Button, Container } from "@mui/material";
+import { Subtitle, Text } from "../components/Typography";
+
 function NotFoundView() {
   return (
-    <div>
-      <h1>404 - Not Found</h1>
-    </div>
+    <Container>
+      <Box my={5}>
+        <Subtitle>Not Found</Subtitle>
+
+        <Box my={3}>
+          <Text>
+            Unfortunately, the page you are looking for does not exist.
+          </Text>
+        </Box>
+
+        <Button variant="contained" color="primary" href="/">
+          Go back to the start page
+        </Button>
+      </Box>
+    </Container>
   );
 }
 NotFoundView.displayName = "NotFoundView";
